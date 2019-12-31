e9b2bd660635988afd099e2a938deb0a2750ba37
Kevin Pilch <me@pilchie.com>
2019-12-31 15:40:05 -0800

    Day 13, Part 1

--- 2019/13/care_package/src/lib.rs
+++ 2019/13/care_package/src/lib.rs
@@ -1,3 +1,89 @@
+use std::collections::HashMap;
+use std::convert::TryInto;
+use std::hash::{Hash, Hasher};
+use termion::{clear, cursor};
+
 mod intcode;
 
-pub use crate::intcode::IntCode;
+struct Position {
+    x: u16,
+    y: u16,
+}
+
+impl PartialEq for Position {
+    fn eq(&self, rhs: &Position) -> bool {
+        return self.x == rhs.x && self.y == rhs.y;
+    }
+}
+
+impl Eq for Position {}
+
+impl Hash for Position {
+    fn hash<H: Hasher>(&self, h: &mut H) {
+        h.write_u16(self.x);
+        h.write_u16(self.y);
+    }
+}
+struct TerminalInputOutputSystem {
+    places: HashMap<Position, char>,
+    value1: Option<i64>,
+    value2: Option<i64>,
+}
+
+impl intcode::InputOutputSystem for TerminalInputOutputSystem {
+    fn print_output(&mut self, value: i64) {
+        match self.value1 {
+            None => self.value1 = Some(value),
+            Some(v1) => {
+                match self.value2 {
+                    None => self.value2 = Some(value),
+                    Some(v2) => {
+                        let x = (v1 + 1).try_into().unwrap();
+                        let y = (v2 + 1).try_into().unwrap();
+                        let tile = TerminalInputOutputSystem::map_tile(value);
+                        print!("{}{}", cursor::Goto(x, y), tile);
+                        self.value1 = None;
+                        self.value2 = None;
+                        self.places.insert(Position{ x, y }, tile);
+                    }
+                }
+            }
+        }
+    }
+
+    fn get_input(&mut self) -> i64 {
+        0
+    }
+}
+
+impl TerminalInputOutputSystem {
+    fn new() -> TerminalInputOutputSystem {
+        print!("{}", clear::All);
+        TerminalInputOutputSystem {
+            places: HashMap::new(),
+            value1: None,
+            value2: None,
+        }
+    }
+
+    fn map_tile(v: i64) -> char {
+        match v {
+            0 => ' ',
+            1 => '█',
+            2 => '#',
+            3 => '-',
+            4 => '⚫',
+            _ => panic!("unexpected tile")
+        }
+    }
+}
+
+pub fn play_game(program: &mut Vec<i64>) {
+    let mut io = TerminalInputOutputSystem::new();
+    let mut computer = intcode::IntCode::new(&mut io);
+    computer.run_to_completion(program);
+    println!();
+
+    let count_blocks = io.places.values().filter(|&c| c == &'#').count();
+    println!("Count of blocks: {}", count_blocks)
+}
--- 2019/13/care_package/src/main.rs
+++ 2019/13/care_package/src/main.rs
@@ -1,3 +1,22 @@
+use care_package::play_game;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut program = parse_input_file(&args[1]);
+    play_game(&mut program);
+}
+
+
+fn parse_input_file(input_filename: &str) -> Vec<i64> {
+    let contents = fs::read_to_string(input_filename).expect("Something went wrong reading the file");
+
+    let mut input: Vec<i64> = Vec::new();
+    for i in contents.split(',') {
+        input.push(i.parse::<i64>().unwrap());
+    }
+
+    input
 }
