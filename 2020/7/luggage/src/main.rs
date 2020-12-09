d4db3b6cbf62111785618e8f3b8f0d72012fed5b
Kevin Pilch <me@pilchie.com>
2020-12-08 16:46:04 -0800

    Get Part 1 to work

--- 2020/7/luggage/src/main.rs
+++ 2020/7/luggage/src/main.rs
@@ -1,25 +1,24 @@
-use std::collections::HashMap;
 use std::collections::HashSet;
+use std::env;
+use std::fs;
+use std::hash::{Hash, Hasher};
 
-fn main() {
-    let rules = Rules::parse(
-        "light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.",
-    );
+fn main() -> Result<(), std::io::Error> {
+    let args : Vec<_> = env::args().collect();
+    let input = fs::read_to_string(&args[1])?;
+    let rules = Rules::parse(&input);
 
-    let r = rules.can_eventually_contain("shiny gold");
-    println!("There are {} ways to contain shiny gold", r.len());
+    if let Some(r) = rules.can_eventually_contain("shiny gold") {
+        println!("There are {} ways to contain shiny gold", r.len());
+    } else {
+        println!("Failed to find any rules that contain shiny gold");
+    }
+
+    Ok(())
 }
 
 pub struct Rules<'a> {
-    rules: HashMap<&'a str, Rule<'a>>,
+    rules: HashSet<Rule<'a>>,
 }
 
 impl<'a> Rules<'a> {
@@ -27,42 +26,45 @@ impl<'a> Rules<'a> {
         Rules {
             rules: input
                 .split('\n')
-                .map(|line| {
-                    let rule = Rule::parse(&line);
-                    (rule.name, rule)
-                })
+                .map(|line| Rule::parse(&line))
                 .collect(),
         }
     }
 
-    pub fn can_eventually_contain(&self, bag_name: &str) -> Vec<&'a Rule> {
-        let r = &self.rules[bag_name];
-        let v: Vec<&Rule> = [r].into();
-
-        self.can_contain_recursive(&v)
+    pub fn can_eventually_contain(&self, bag_name: &str) -> Option<HashSet<&'a Rule>> {
+        if let Some(r) = self.rules.iter().filter(|x| x.name == bag_name).nth(0) {
+            let mut v = HashSet::new();
+            v.insert(r);
+            v = self.can_contain_recursive(v, 0);
+            v.remove(r);
+            Some(v)
+        } else {
+            None
+        }
     }
 
-    fn can_contain_recursive(&self, rules: &[&'a Rule]) -> Vec<&'a Rule> {
-        println!("Looking for rules that can contain {:?}", rules);
-        let mut new = Vec::new();
-        for (n, r1) in &self.rules {
-            for r2 in rules {
-                if n != &r2.name {
-                    if r1.can_contain_directly(r2.name) {
-                        new.push(r1);
-                    }
+    fn can_contain_recursive(&self, rules: HashSet<&'a Rule<'a>>, depth: usize) -> HashSet<&'a Rule> {
+        let names : Vec<&'a str> = rules.iter().map(|r| r.name).collect();
+        println!("Looking for rules that can contain {:?}", names);
+        let mut new = HashSet::new();
+        for r1 in &self.rules {
+            for r2 in &rules {
+                if r1.can_contain_directly(r2.name) {
+                    new.insert(r1);
                 }
             }
         }
 
-        if new.is_empty() {
-            return rules.into();
+        let len = rules.len();
+        for r in rules {
+            new.insert(r);
         }
 
-        for r in rules {
-            new.push(r);
+        if new.len() == len {
+            return new
         }
-        self.can_contain_recursive(&new)
+
+        self.can_contain_recursive(new, depth + 1)
     }
 }
 
@@ -110,6 +112,21 @@ impl<'a> Rule<'a> {
     }
 }
 
+impl<'a> PartialEq for Rule<'a> {
+    fn eq(&self, rhs : &Rule<'a>) -> bool {
+        self.name == rhs.name
+    }
+}
+
+impl<'a> Eq for Rule<'a> {
+}
+
+impl<'a> Hash for Rule<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -127,6 +144,13 @@ vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.",
         );
-        assert_eq!(4, rules.can_eventually_contain("shiny gold").len());
+
+        if let Some(x) = rules.can_eventually_contain("shiny gold") {
+            assert_eq!(4, x.len());
+        } else {
+            unreachable!();
+        }
     }
 }
+
+
