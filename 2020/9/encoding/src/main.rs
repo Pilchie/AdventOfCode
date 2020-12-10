762dc3adeee572cf23a261639027f063006e8178
Kevin Pilch <me@pilchie.com>
2020-12-09 19:12:59 -0800

    Day 9, part 2

--- 2020/9/encoding/src/main.rs
+++ 2020/9/encoding/src/main.rs
@@ -7,10 +7,10 @@ fn main() -> Result<(), Error> {
     let reader = BufReader::new(File::open(&args[1])?);
 
     let input : Vec<_> = reader.lines().map(|line| line.unwrap().parse::<usize>().unwrap()).collect();
-    if let Some(first_invalid) = first_invalid(&input, 25) {
-        println!("Found {} as first invalid", first_invalid);
+    if let Some(weakness) = encryption_weakness(&input, 25) {
+        println!("Found encryption weakness: {}", weakness);
     } else {
-        println!("Didn't find an invalid input");
+        println!("Didn't find an encryption weakness.");
     }
 
     Ok(())
@@ -31,16 +31,33 @@ pub fn is_last_valid(num: usize, input: &[usize]) -> bool {
     false
 }
 
-pub fn first_invalid(input: &[usize], preamble_length: usize) -> Option<usize> {
+pub fn first_invalid(input: &[usize], preamble_length: usize) -> Option<(usize, usize)> {
     // Consider - storing a set of previous sums, along with the indices of their addends would allow
     // fewer iterations if the input was super long, or the preamble length was longer.
     for x in preamble_length..input.len() {
         //println!("looking at index {} from {} to {}", x, x-preamble_length, x);
         if !is_last_valid(input[x], &input[x-preamble_length..x]) {
-            return Some(input[x]);
+            return Some((x,input[x]));
         }
     }
 
+    None
+}
+
+pub fn encryption_weakness(input: &[usize], preamble_size: usize) -> Option<usize> {
+    let (index, first_invalid) = first_invalid(input, preamble_size)?;
+
+    // Let's just brute force this - how many subsets can there be?
+    for x in 0..index-1 {
+        for y in x+1..index {
+            let val = input[x..y].iter().fold(0, |acc, v| acc+v);
+            if val == first_invalid {
+                let min = input[x..y].iter().min()?;
+                let max = input[x..y].iter().max()?;
+                return Some(min + max);
+            }
+        }
+    }
     None
 }
 
@@ -71,12 +88,18 @@ mod tests {
     #[test]
     fn test_sequence() {
         let input = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,];
-        assert_eq!(Some(127), first_invalid(&input, 5));
+        assert_eq!(Some((14, 127)), first_invalid(&input, 5));
     }
 
     #[test]
     fn from_input() {
         let input = [175, 182, 209, 191, 192, 195, 206, 297, 215, 218, 229, 265, 257, 262, 287, 368, 293, 302, 321, 331, 371, 338, 392, 367, 439, 542,];
         assert!(is_last_valid(863, &input));
+    }
+
+    #[test]
+    fn weakness() {
+        let input = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,];
+        assert_eq!(Some(62), encryption_weakness(&input, 5));
     }
 }
