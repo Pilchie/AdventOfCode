d983a3f824340f06adc7d97032f4eafcf8b2517a
Kevin Pilch <me@pilchie.com>
2020-12-09 16:57:22 -0800

    Day 8, part 2

--- 2020/8/handheld/src/main.rs
+++ 2020/8/handheld/src/main.rs
@@ -6,8 +6,22 @@ use std::num::ParseIntError;
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let input = fs::read_to_string(&args[1])?;
-    if let Ok(x) = Emulator::run(&input) {
-        println!("Acc is {} before looping", x);
+    if let Ok(program) = Program::parse(&input) {
+        for x in 0..program.len() {
+            let mut modified_program = program.to_vec();
+            let new_instr = match program[x] {
+                Instruction::Nop(x) => Instruction::Jmp(x),
+                Instruction::Jmp(x) => Instruction::Nop(x),
+                Instruction::Acc(x) => Instruction::Acc(x),
+            };
+            modified_program[x] = new_instr;
+            let (acc, pc) = Emulator::run(&modified_program);
+            if pc == program.len() {
+                println!("Completed the program with acc value {}", acc);
+                break;
+            }
+
+        }
     } else {
         println!("Didn't run");
     }
@@ -15,21 +29,28 @@ fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-pub struct Emulator {}
-
-impl Emulator {
-    pub fn run(lines: &str) -> Result<isize, Error> {
+pub struct Program {}
+impl Program {
+    pub fn parse(lines: &str) -> Result<Vec<Instruction>, Error> {
         let mut program = Vec::new();
         for line in lines.lines() {
             let instr = Instruction::parse(line)?;
             program.push(instr);
         }
 
+        Ok(program)
+    }
+}
+
+pub struct Emulator {}
+
+impl Emulator {
+    pub fn run(program: &[Instruction]) -> (isize, usize) {
         let mut seen = HashSet::new();
-        let mut pc: isize = 0;
+        let mut pc = 0;
         let mut acc = 0;
 
-        while !seen.contains(&pc) {
+        while !seen.contains(&pc) && pc < program.len() as isize {
             seen.insert(pc);
             let instr = &program[pc as usize];
             let (a, p) = instr.exec();
@@ -37,7 +58,7 @@ impl Emulator {
             pc += p;
         }
 
-        Ok(acc)
+        (acc, pc as usize)
     }
 }
 
@@ -53,10 +74,11 @@ impl From<ParseIntError> for Error {
 }
 
 #[derive(Debug)]
+#[derive(Clone)]
 pub enum Instruction {
     Acc(isize),
     Jmp(isize),
-    Nop,
+    Nop(isize),
 }
 
 impl Instruction {
@@ -65,7 +87,7 @@ impl Instruction {
         let val = parts[1].parse::<isize>()?;
 
         match parts[0] {
-            "nop" => Ok(Instruction::Nop),
+            "nop" => Ok(Instruction::Nop(0)),
             "acc" => Ok(Instruction::Acc(val)),
             "jmp" => Ok(Instruction::Jmp(val)),
             _ => Err(Error::UnexpectedInstruction),
@@ -74,7 +96,7 @@ impl Instruction {
 
     pub fn exec(&self) -> (isize, isize) {
         match self {
-            Instruction::Nop => (0, 1),
+            Instruction::Nop(_) => (0, 1),
             Instruction::Acc(x) => (*x, 1),
             Instruction::Jmp(x) => (0, *x),
         }
@@ -87,7 +109,7 @@ mod tests {
 
     #[test]
     pub fn first() {
-        let program = "nop +0  | 1
+        let input = "nop +0  | 1
 acc +1
 jmp +4
 acc +3
@@ -97,8 +119,9 @@ acc +1
 jmp -4
 acc +6";
 
-        if let Ok(x) = Emulator::run(program) {
-            assert_eq!(5, x);
+        if let Ok(program) = Program::parse(input) {
+            let (acc, _) = Emulator::run(&program);
+            assert_eq!(5, acc);
         }
     }
 }
