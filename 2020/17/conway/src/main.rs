e18da8b841f8ff4e3fae9b22f4684e1dee540107
Kevin Pilch <me@pilchie.com>
2020-12-17 18:37:05 +0000

    Scaffold Day 17

---
+++ 2020/17/conway/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world!");
+
+    let input = "#.#####.
+#..##...
+.##..#..
+#.##.###
+.#.#.#..
+#.##..#.
+#####..#
+..#.#.##";
+}
+
+#[cfg(test)]
+mod tests_part1 {
+    use super::*;
+
+    #[test]
+    fn test() {
+
+    }
+}
