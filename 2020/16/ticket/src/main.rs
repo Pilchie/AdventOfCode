a17bd35f5fd49dedb24e48b10a3b520330f43ffb
Kevin Pilch <me@pilchie.com>
2020-12-17 18:23:13 +0000

    Day 16, part 2
    * Switch to usize instead of i32
    * Fix typo, if you start a product with 0, bad things will happen
    * Switch to fixing the items with a single possible field. The hint was
      when I added an ouput of the count of possible combos without deduping
      and realized it was equal to 20!

--- 2020/16/ticket/src/main.rs
+++ 2020/16/ticket/src/main.rs
@@ -7,18 +7,15 @@ fn main() -> Result<(), std::io::Error> {
     let conditions = ConditionSet::parse(&input);
     let tickets = Ticket::parse_nearby(&input);
     if let Some(mine) = Ticket::parse_yours(&input) {
-        if let Some(res) = conditions.order_fields(&tickets) {
-            let mut product = 0;
-            for (name, _) in conditions.fields {
-                if name.starts_with("departure") {
-                    product *= mine.field_values[res[&name]];
-                }
+        let res = conditions.order_fields(&tickets);
+        let mut product = 1;
+        for (name, _) in &conditions.fields {
+            if name.starts_with("departure") {
+                product *= mine.field_values[res[&name]];
             }
-
-            println!("The product is: {}", product);
-        } else {
-            println!("Didn't find a solution")
         }
+
+        println!("The product is: {}", product);
     } else {
         println!("Couldn't parse my ticket")
     }
@@ -28,8 +25,8 @@ fn main() -> Result<(), std::io::Error> {
 
 #[derive(Debug)]
 pub struct Range {
-    min: i32,
-    max: i32,
+    min: usize,
+    max: usize,
 }
 
 impl Range {
@@ -40,7 +37,7 @@ impl Range {
         }
     }
 
-    fn in_ranges(v: i32, r1: &Range, r2: &Range) -> bool {
+    fn in_ranges(v: usize, r1: &Range, r2: &Range) -> bool {
         v >= r1.min && v <= r1.max || v >= r2.min && v <= r2.max
     }
 }
@@ -69,7 +66,7 @@ impl ConditionSet {
         ConditionSet { fields: fields }
     }
 
-    pub fn invalid_field_values(&self, ticket: &Ticket) -> Vec<i32> {
+    pub fn invalid_field_values(&self, ticket: &Ticket) -> Vec<usize> {
         let mut res = Vec::new();
 
         for v in &ticket.field_values {
@@ -108,7 +105,7 @@ impl ConditionSet {
         true
     }
 
-    pub fn order_fields<'a>(&self, tickets: &[Ticket]) -> Option<HashMap<String, usize>> {
+    pub fn order_fields<'a>(&self, tickets: &[Ticket]) -> HashMap<&String, usize> {
         let valid_tickets: Vec<_> = tickets.iter().filter(|t| self.is_possibly_valid(t)).collect();
 
         let mut valid_positions_by_name = HashMap::new();
@@ -120,131 +117,38 @@ impl ConditionSet {
             }
         }
 
-        let mut p = 1;
-        for (name, positions) in &valid_positions_by_name {
-            p *= positions.len();
-            println!("Possible for {} ({}): - {:?}", name, positions.len(), positions);
-        }
-        println!("Expecting up to {} combos", p);
-
-        let mut possible_indices = Vec::new();
-        let mut names = Vec::new();
-        for (name, positions) in &valid_positions_by_name {
-            names.push(*name);
-            possible_indices.push(positions.clone());
+        let mut res = HashMap::new();
+        while !valid_positions_by_name.is_empty() {
+            valid_positions_by_name = Self::fix_single_position(&mut res, & valid_positions_by_name);
         }
 
-        let possible_indices = Self::generate_possible_indices(&possible_indices);
-        println!("Trying {} possible combinations", possible_indices.len());
-        for indices in possible_indices {
-            if self.validate_indices(&names, &indices, &valid_tickets) {
-                let mut res = HashMap::new();
-                for i in 0..names.len() {
-                    res.insert(names[i].clone(), indices[i]);
-                }
-                return Some(res)
-            }
-        }
-
-        None
+       res
     }
 
-    fn validate_indices<'a> (&self,
-        names: &Vec<&String>,
-        indices: &[usize],
-        valid_tickets: &Vec<&Ticket>) -> bool
-    {
-        print!("Validating ");
-        for i in 0..names.len() {
-            if let Some((r1, r2)) = self.fields.get(names[i]) {
-                let x = indices[i];
-                println!("{}:{} in ({}-{},{}-{}) ", names[i], x, r1.min, r1.max, r2.min, r2.max);
-                for t in valid_tickets {
-                    print!("{} ", t.field_values[x]);
-                    if !Range::in_ranges(t.field_values[x], r1, r2) {
-                        println!("FALSE");
-                        return false;
+    fn fix_single_position<'a>(res: &mut HashMap<&'a String, usize>, valid_positions_by_name: &HashMap<&'a String, Vec<usize>>) ->  HashMap<&'a String, Vec<usize>>{
+        let mut x = HashMap::new();
+
+        for (name, positions) in valid_positions_by_name {
+            if positions.len() == 1 {
+                res.insert(name, positions[0]);
+
+                for (n, p2) in valid_positions_by_name {
+                    if n != name {
+                        let mut p = p2.clone();
+                        vec_remove_item(&mut p, &positions[0]);
+                        x.insert(*n, p);
                     }
                 }
-                println!("");
-            } else {
-                panic!("Should been an entry for {}", names[i]);
+                break;
             }
         }
 
-        println!("TRUE");
-        true
-    }
-
-    fn _order_example<'a>(
-        &self,
-        valid_positions_by_name: &'a HashMap<&'a String, Vec<usize>>,
-        valid_tickets: &Vec<&Ticket>) -> Option<HashMap<String, usize>>
-    {
-        let ck = &String::from("class");
-        let rk = &String::from("row");
-        let sk = &String::from("seat");
-        for cp in &valid_positions_by_name[ck] {
-            for rp in &valid_positions_by_name[rk] {
-                for sp in &valid_positions_by_name[sk] {
-                    let (rc1, rc2) = &self.fields[ck];
-                    let (rr1, rr2) = &self.fields[rk];
-                    let (rs1, rs2) = &self.fields[sk];
-
-                    if valid_tickets.iter().all(
-                        |t| Range::in_ranges(t.field_values[*cp], &rc1, &rc2)
-                        && Range::in_ranges(t.field_values[*rp], &rr1, &rr2)
-                        && Range::in_ranges(t.field_values[*sp], &rs1, &rs2))
-                        {
-                            let mut res = HashMap::new();
-                            res.insert(ck.clone(), *cp);
-                            res.insert(rk.clone(), *rp);
-                            res.insert(sk.clone(), *sp);
-                            return Some(res);
-                        }
-                }
-            }
-        }
-
-        None
-    }
-
-    fn generate_possible_indices<T: Copy+std::fmt::Debug+PartialEq>(x: &[Vec<T>]) -> Vec<Vec<T>> {
-        if x.len() == 1 {
-            let mut res = Vec::new();
-            for item in &x[0] {
-                res.push(std::iter::once(*item).collect());
-            }
-
-            return res;
-        }
-
-        let mut appended: Vec<Vec<T>> = Vec::new();
-        for s_el in &x[0] {
-            for next_s in Self::generate_possible_indices(&x[1..]) {
-                let mut a = Vec::new();
-                a.push(*s_el);
-                let mut push = true;
-                for n in next_s {
-                    if a.contains(&n) {
-                        push = false;
-                        break;
-                    } else {
-                        a.push(n);
-                    }
-                }
-
-                if push {
-                    appended.push(a);
-                }
-            }
-        }
-        appended
+        x
     }
 }
 
 pub struct Ticket {
-    field_values: Vec<i32>,
+    field_values: Vec<usize>,
 }
 
 impl Ticket {
@@ -254,7 +158,7 @@ impl Ticket {
         for line in input.lines() {
             if start {
                 res.push(Ticket {
-                    field_values: line.split(',').map(|s| s.parse::<i32>().unwrap()).collect(),
+                    field_values: line.split(',').map(|s| s.parse::<usize>().unwrap()).collect(),
                 });
             }
 
@@ -270,7 +174,7 @@ impl Ticket {
         for line in input.lines() {
             if start {
                 return Some(Ticket {
-                    field_values: line.split(',').map(|s| s.parse::<i32>().unwrap()).collect(),
+                    field_values: line.split(',').map(|s| s.parse::<usize>().unwrap()).collect(),
                 });
             }
 
@@ -288,6 +192,14 @@ fn split_once<'a>(in_string: &'a str, split_on: &str) -> (&'a str, &'a str) {
     let first = splitter.next().unwrap();
     let second = splitter.next().unwrap();
     (first, second)
+}
+
+fn vec_remove_item<T: PartialEq>(vec: &mut Vec<T>, item: &T) -> Option<T> {
+    let pos = match vec.iter().position(|x| *x == *item) {
+        Some(x) => x,
+        None => return None,
+    };
+    Some(vec.remove(pos))
 }
 
 #[cfg(test)]
@@ -312,7 +224,7 @@ nearby tickets:
         let conditions = ConditionSet::parse(input);
         let tickets = Ticket::parse_nearby(input);
 
-        assert_eq!(71, tickets.iter().flat_map(|t| conditions.invalid_field_values(t)).sum());
+        assert_eq!(71, tickets.iter().flat_map(|t| conditions.invalid_field_values(t)).fold(0, |acc, n| acc + n));
     }
 }
 
@@ -323,8 +235,8 @@ mod tests_part2 {
     #[test]
     fn test() {
         let input = "class: 0-1 or 4-19
-seat: 0-13 or 16-19
 row: 0-5 or 8-19
+seat: 0-13 or 16-19
 
 your ticket:
 11,12,13
@@ -337,26 +249,9 @@ nearby tickets:
         let conditions = ConditionSet::parse(input);
         let tickets = Ticket::parse_nearby(input);
 
-        if let Some(res) = conditions.order_fields(&tickets) {
-            assert_eq!(0, res["row"]);
-            assert_eq!(1, res["class"]);
-            assert_eq!(2, res["seat"]);
-        } else {
-            assert!(false);
-        }
-    }
-
-    #[test]
-    pub fn test_generated_indices() {
-        let mut sets = Vec::new();
-        sets.push([1, 2].to_vec());
-        sets.push([2, 4].to_vec());
-        sets.push([4, 6].to_vec());
-
-        let res = ConditionSet::generate_possible_indices(&sets);
-        for x in res {
-            println!("{:?}", x);
-        }
-        assert!(false);
+        let res = conditions.order_fields(&tickets);
+        assert_eq!(0, res[&String::from("row")]);
+        assert_eq!(1, res[&String::from("class")]);
+        assert_eq!(2, res[&String::from("seat")]);
     }
 }
