4874699dc524de8fcc55e184ee8ec38f6d4f0e67
Kevin Pilch <me@pilchie.com>
2020-12-19 05:25:04 +0000

    Day 18, part 2

--- 2020/18/operation/src/main.rs
+++ 2020/18/operation/src/main.rs
@@ -1,6 +1,5 @@
 use std::io::{BufRead, BufReader};
 
-
 fn main() -> Result<(), Error> {
     let args: Vec<_> = std::env::args().collect();
     let reader = BufReader::new(std::fs::File::open(&args[1])?);
@@ -19,69 +18,92 @@ pub fn evaluate(input: &str) -> Result<isize, Error> {
     Ok(res)
 }
 
-fn evaluate_helper(input: &str) ->  Result<(isize, &str), Error> {
-    let (mut left, mut remaining) = parse_term(&input)?;
+fn evaluate_helper(input: &str) -> Result<(isize, &str), Error> {
+    let (mut left, mut remaining) = parse_product(input)?;
 
-    while remaining.len() > 0 {
-        if &remaining[0..1] == ")" {
-            return Ok((left, &remaining[1..]));
+    while !remaining.is_empty() {
+        let t = peek_token(remaining)?;
+        match t {
+            Token::Multiplication => {
+                let (right, r) = parse_product(&remaining[3..])?;
+                remaining = r;
+                left = left * right;
+            }
+            Token::CloseParen => {
+                return Ok((left, &remaining[1..]));
+            }
+            _ => {
+                return Err(Error::UnexpectedToken(t));
+            }
         }
-
-        let (operator, r1) = parse_operator(&remaining)?;
-        let (right, r2) = parse_term(&r1)?;
-        remaining = r2;
-        left = operator.apply(left, right);
     }
 
     Ok((left, remaining))
 }
 
-pub fn parse_term(input: &str) -> Result<(isize, &str), Error> {
-    println!("parse_term, input: {}", input);
+fn parse_product(input: &str) -> Result<(isize, &str), Error> {
+    let (mut left, mut remaining) = parse_term(input)?;
+    loop {
+        let t = peek_token(remaining)?;
+        match t {
+            Token::Addition => {
+                let (right, rem) = parse_term(&remaining[3..])?;
+                remaining = rem;
+                left += right;
+            }
+            Token::Multiplication => return Ok((left, remaining)),
+            Token::CloseParen => return Ok((left, remaining)),
+            Token::EOF => return Ok((left, remaining)),
+            _ => return Err(Error::UnexpectedToken(t)),
+        }
+    }
+}
 
-    match &input[0..1] {
-        "0" => Ok((0, &input[1..])),
-        "1" => Ok((1, &input[1..])),
-        "2" => Ok((2, &input[1..])),
-        "3" => Ok((3, &input[1..])),
-        "4" => Ok((4, &input[1..])),
-        "5" => Ok((5, &input[1..])),
-        "6" => Ok((6, &input[1..])),
-        "7" => Ok((7, &input[1..])),
-        "8" => Ok((8, &input[1..])),
-        "9" => Ok((9, &input[1..])),
-        "(" => {
-            let (res, rem) = evaluate_helper(&input[1..])?;
-            Ok((res, rem))
+fn parse_term(input: &str) -> Result<(isize, &str), Error> {
+    let t = peek_token(input)?;
+    match t {
+        Token::OpenParen => evaluate_helper(&input[1..]),
+        Token::Number(num) => Ok((num, &input[1..])),
+        _ => Err(Error::UnexpectedToken(t)),
+    }
+}
+
+fn peek_token(input: &str) -> Result<Token, Error> {
+    for c in input.bytes() {
+        if c == b' ' {
+            continue;
         }
 
-        _ => Err(Error::InvalidTerm),
+        return match c {
+            b'+' => Ok(Token::Addition),
+            b'*' => Ok(Token::Multiplication),
+            b'(' => Ok(Token::OpenParen),
+            b')' => Ok(Token::CloseParen),
+            b'0' => Ok(Token::Number(0)),
+            b'1' => Ok(Token::Number(1)),
+            b'2' => Ok(Token::Number(2)),
+            b'3' => Ok(Token::Number(3)),
+            b'4' => Ok(Token::Number(4)),
+            b'5' => Ok(Token::Number(5)),
+            b'6' => Ok(Token::Number(6)),
+            b'7' => Ok(Token::Number(7)),
+            b'8' => Ok(Token::Number(8)),
+            b'9' => Ok(Token::Number(9)),
+            _ => Err(Error::InvalidOperator {}),
+        };
     }
 
+    Ok(Token::EOF)
 }
 
-pub fn parse_operator(input: &str) -> Result<(Operator, &str), Error> {
-    println!("parse_operator, input: {}", input);
-
-    match &input[0..3] {
-        " + " => Ok((Operator::Addition, &input[3..])),
-        " * " => Ok((Operator::Multiplication, &input[3..])),
-        _ => Err(Error::InvalidOperator {}),
-    }
-}
-
-pub enum Operator {
+#[derive(Debug, PartialEq)]
+pub enum Token {
     Addition,
     Multiplication,
-}
-
-impl Operator {
-    pub fn apply(&self, left: isize, right: isize) -> isize {
-        match self {
-            Operator::Addition => left + right,
-            Operator::Multiplication => left * right,
-        }
-    }
+    OpenParen,
+    CloseParen,
+    Number(isize),
+    EOF,
 }
 
 #[derive(Debug, PartialEq)]
@@ -89,6 +111,7 @@ pub enum Error {
     InvalidOperator,
     InvalidTerm,
     IO,
+    UnexpectedToken(Token),
 }
 
 impl std::convert::From<std::io::Error> for Error {
@@ -98,12 +121,12 @@ impl std::convert::From<std::io::Error> for Error {
 }
 
 #[cfg(test)]
-mod tests_part1 {
+mod tests_part2 {
     use super::*;
 
     #[test]
     fn example1() {
-        assert_eq!(Ok(71), evaluate("1 + 2 * 3 + 4 * 5 + 6"));
+        assert_eq!(Ok(231), evaluate("1 + 2 * 3 + 4 * 5 + 6"));
     }
 
     #[test]
@@ -113,22 +136,39 @@ mod tests_part1 {
 
     #[test]
     fn example3() {
-        assert_eq!(Ok(26), evaluate("2 * 3 + (4 * 5)"));
+        assert_eq!(Ok(46), evaluate("2 * 3 + (4 * 5)"));
     }
 
     #[test]
     fn example4() {
-        assert_eq!(Ok(437), evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        assert_eq!(Ok(1445), evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
     }
 
     #[test]
     fn example5() {
-        assert_eq!(Ok(12240), evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"));
+        assert_eq!(
+            Ok(669060),
+            evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+        );
     }
 
-    
     #[test]
     fn example6() {
-        assert_eq!(Ok(13632), evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
+        assert_eq!(
+            Ok(23340),
+            evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+        );
+        //                              ((  6   * 9) * ( 15   *   14 ) + 6) + 2 + 4 * 2
+        //                              (     54     *        210      + 6) + 2 + 4 * 2
+        //                              (     54     *        216)          + 2 + 4 * 2
+        //                                         11,664                   + 2 + 4 * 2
+        //                                                          11,666      + 4 * 2
+        //                                                                  11,670  * 2
+        //                                                                       23,340
+    }
+
+    #[test]
+    fn mine() {
+        assert_eq!(Ok(480), evaluate("8 * 3 + 9 + 3 * 4"));
     }
 }
