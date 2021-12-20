80243b968287c0e94281e5657bb8e49dc163ea54
Kevin Pilch <me@pilchie.com>
2021-12-20 00:33:45 +0000

    2021-12-09 - Part 2

--- 2021/9/smoke_basin/src/main.rs
+++ 2021/9/smoke_basin/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Debug)]
 enum Error {
     IO(std::io::Error),
@@ -21,20 +23,31 @@ fn main() -> Result<(), Error> {
     let input = std::fs::read_to_string(&args[1])?;
 
     let map = HeightMap::parse(&input);
-
-    let mut res = 0;
+    let mut sizes = Vec::new();
 
     for y in 0..map.points.len() {
         for x in 0..map.points[y].len() {
             if map.is_low_point(x, y) {
-                let risk = map.risk_level(x, y);
-                println!("Found low point at ({},{}) with risk {}", y, x, risk);
-                res += risk;
+                let size = map.find_basin_size(x, y);
+                sizes.push(size);
+                println!("Found low point at ({},{}) with basin size {}.", y, x, size);
             }
         }
     }
 
-    println!("The sum of risk levels of low points is {}", res);
+    sizes.sort();
+    sizes.reverse();
+    let mut res = 1;
+
+    for (i, s) in sizes.iter().enumerate() {
+        if i > 2 {
+            break;
+        }
+
+        res *= s;
+    }
+
+    println!("The product of the basin sizes is {}", res);
 
     Ok(())
 }
@@ -46,13 +59,12 @@ struct HeightMap {
 impl HeightMap {
     fn parse(input: &str) -> Self {
         let mut res = Vec::new();
+        let a = "0".bytes().next().unwrap();
         for line in input.lines() {
-            let row : Vec<_> = line.bytes().collect();
+            let row: Vec<_> = line.bytes().map(|b| b - a).collect();
             res.push(row);
         }
-        Self {
-            points: res,
-        }
+        Self { points: res }
     }
 
     fn is_low_point(self: &Self, x: usize, y: usize) -> bool {
@@ -69,7 +81,7 @@ impl HeightMap {
         }
 
         // below
-        if y < self.points.len() - 1 && val > self.points[y+1][x] {
+        if y < self.points.len() - 1 && val > self.points[y + 1][x] {
             return false;
         }
 
@@ -82,7 +94,84 @@ impl HeightMap {
     }
 
     fn risk_level(self: &Self, x: usize, y: usize) -> u32 {
-        let a = "0".bytes().next().unwrap();
-        (self.points[y][x] - a + 1).into()
+        (self.points[y][x] + 1).into()
     }
+
+    fn find_basin_size(self: &Self, x: usize, y: usize) -> usize {
+        let mut explored = HashSet::new();
+        let mut basin = HashSet::new();
+        let p = Point { x, y };
+        basin.insert(p);
+
+        while basin.len() != explored.len() {
+            self.explore_basin(&mut basin, &mut explored)
+        }
+
+        basin.len()
+    }
+
+    fn explore_basin(self: &Self, basin: &mut HashSet<Point>, explored: &mut HashSet<Point>) {
+        let mut new = HashSet::new();
+        for p in basin.iter() {
+            if !explored.contains(p) {
+                explored.insert(*p);
+
+                // left
+                for x in 1..p.x + 1 {
+                    //print!("Checking ({},{})...", p.y, p.x - x);
+                    if p.x >= x && self.points[p.y][p.x - x] < 9 {
+                        //println!("Added");
+                        new.insert(Point { x: p.x - x, y: p.y });
+                    } else {
+                        //println!("Stopping");
+                        break;
+                    }
+                }
+
+                // right
+                for x in (p.x + 1)..self.points[p.y].len() {
+                    //print!("Checking ({},{})...", p.y, x);
+                    if x < self.points[p.y].len() && self.points[p.y][x] < 9 {
+                        //println!("Added");
+                        new.insert(Point { x: x, y: p.y });
+                    } else {
+                        //println!("Stopping");
+                        break;
+                    }
+                }
+
+                // up
+                for y in 1..p.y + 1 {
+                    //print!("Checking ({},{})...", p.y - y, p.x);
+                    if p.y >= y && self.points[p.y - y][p.x] < 9 {
+                        //println!("Added");
+                        new.insert(Point { x: p.x, y: p.y - y });
+                    } else {
+                        //println!("Stopping");
+                        break;
+                    }
+                }
+
+                // down
+                for y in (p.y + 1)..self.points.len() {
+                    //print!("Checking ({},{})...", y, p.x);
+                    if y < self.points.len() && self.points[y][p.x] < 9 {
+                        //println!("Added");
+                        new.insert(Point { x: p.x, y: y });
+                    } else {
+                        //println!("Stopping");
+                        break;
+                    }
+                }
+            }
+        }
+
+        basin.extend(new);
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct Point {
+    x: usize,
+    y: usize,
 }
