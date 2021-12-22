e26f30ea2daa247f565d97b51df49b3588d8d166
Kevin Pilch <me@pilchie.com>
2021-12-22 19:22:50 +0000

    2021-12-18 - Part 1
    Decided that operating on tokens was easier for the backtracking
    necessary in explode.

--- 2021/18/snailfish/src/main.rs
+++ 2021/18/snailfish/src/main.rs
@@ -2,9 +2,9 @@ fn main() -> Result<(), std::io::Error> {
     let args: Vec<_> = std::env::args().collect();
     let input = std::fs::read_to_string(&args[1])?;
 
-    let mut value = parse_pair(&mut 0, input.lines().nth(0).unwrap());
+    let mut value = Pair::parse_str(input.lines().nth(0).unwrap());
     for line in input.lines().skip(1) {
-        value = add(&value, &parse_pair(&mut 0, line));
+        value = Pair::add(&value, &Pair::parse_str(line));
     }
 
     value.print();
@@ -12,102 +12,6 @@ fn main() -> Result<(), std::io::Error> {
 
     println!("The magnitude is: {}", value.magnitude());
     Ok(())
-}
-
-fn parse_pair(i: &mut usize, input: &str) -> Pair {
-    expect(i, input, "[");
-    let lhs = parse_term(i, input);
-    expect(i, input, ",");
-    let rhs = parse_term(i, input);
-    expect(i, input, "]");
-
-    Pair { lhs, rhs, }
-}
-
-fn parse_term(i: &mut usize, input: &str) -> Term {
-    if peek(i, input, "[") {
-        let pair = parse_pair(i, input);
-        Term::Pair(Box::new(pair))
-    } else {
-        let value = parse_value(i, input);
-        Term::Value(value)
-    }
-}
-
-fn parse_value(i: &mut usize, input: &str) -> i32 {
-    let start = *i;
-    while &input[*i..*i+1] != "," && &input[*i..*i+1] != "]" {
-        *i += 1;
-    }
-
-    let sub = &input[start..*i];
-    sub.parse().unwrap()
-}
-
-fn expect(i: &mut usize, input: &str, expected: &str) {
-    let actual = &input[*i..*i+1];
-    if actual != expected {
-        panic!("Expected {}, got {}", expected, actual);
-    }
-    *i += 1;
-}
-
-fn peek(i: &mut usize, input: &str, expected: &str) -> bool {
-    let actual = &input[*i..*i+1];
-    actual == expected
-}
-
-fn add(lhs: &Pair, rhs: &Pair) -> Pair {
-    let mut res = Pair {
-        lhs: Term::Pair(Box::new(lhs.clone())),
-        rhs: Term::Pair(Box::new(rhs.clone())),
-    };
-
-    res = reduce(&res);
-    res
-}
-
-fn reduce(pair: &Pair) -> Pair {
-    let pairs = 1;
-    let (term, _) = reduce_rec(pair, pairs);
-    if let Term::Pair(p) = term {
-        *p
-    } else {
-        panic!("Unknown")
-    }
-}
-
-fn reduce_rec(pair: &Pair, pairs: u32) -> (Term, bool) {
-    if pairs == 4 {
-        return (Term::Pair(Box::new(explode(pair))), true)
-    }
-
-    let (lhs, stop) = match &pair.lhs {
-        Term::Pair(p) => reduce_rec(&p, pairs + 1),
-        Term::Value(v) => split(*v),
-    };
-    if stop {
-        return (Term::Pair(Box::new(Pair {lhs, rhs: pair.rhs.clone()})), stop);
-    }
-
-    let (rhs, stop) = match &pair.rhs {
-        Term::Pair(p) => reduce_rec(&p, pairs + 1),
-        Term::Value(v) => split(*v),
-    };
-
-    (Term::Pair(Box::new(Pair{lhs, rhs})), stop)
-}
-
-fn split(v: i32) -> (Term, bool) {
-    if v >= 10 {
-        (Term::Pair(Box::new(Pair { lhs: Term::Value(v / 2), rhs: Term::Value(v + 1 / 2) })), true)
-    } else {
-        (Term::Value(v), false)
-    }
-}
-
-fn explode(pair: &Pair) -> Pair {
-    pair.clone()
 }
 
 enum Term {
@@ -121,6 +25,13 @@ impl Term {
             Term::Value(v) => print!("{}", v),
             Term::Pair(p) => p.print(),
         }
+    }
+
+    fn append_to(&self, vec: &mut Vec<Element>) {
+        match self {
+            Term::Value(v) => vec.push(Element::Value(*v)),
+            Term::Pair(p) => p.append_to(vec),
+        }  
     }
 
     fn magnitude(&self) -> i32 {
@@ -147,6 +58,254 @@ struct Pair {
 }
 
 impl Pair {
+    fn parse_str(input: &str) -> Self {
+        let elements = Pair::lex(input);
+
+        let mut i = 0;
+        Pair::parse(&mut i, &elements)
+    }
+
+    fn parse(i: &mut usize, input: &Vec<Element>) -> Self {
+        Pair::expect(i, input, &Element::Open);
+        let lhs = Pair::parse_term(i, input);
+        Pair::expect(i, input, &Element::Comma);
+        let rhs = Pair::parse_term(i, input);
+        Pair::expect(i, input, &Element::Close);
+    
+        Self { lhs, rhs, }
+    }
+
+    fn parse_term(i: &mut usize, input: &Vec<Element>) -> Term {
+        if Pair::peek(i, input, &Element::Open) {
+            let pair = Pair::parse(i, input);
+            Term::Pair(Box::new(pair))
+        } else {
+            let value = Pair::parse_value(i, input);
+            Term::Value(value)
+        }
+    }
+
+    fn parse_value(i: &mut usize, input: &Vec<Element>) -> i32 {
+        let res = match input[*i] {
+            Element::Value(value) => value,
+            _ => panic!("Unexpected input element, expected a value, got {:?}", input[*i]),
+        };
+        *i += 1;
+        res
+    }
+
+    fn expect(i: &mut usize, input: &Vec<Element>, expected: &Element) {
+        let actual = &input[*i];
+        if actual != expected {
+            panic!("Expected {:?}, got {:?}", expected, actual);
+        }
+        *i += 1;
+    }
+    
+    fn peek(i: &mut usize, input: &Vec<Element>, expected: &Element) -> bool {
+        let actual = &input[*i];
+        actual == expected
+    }
+
+    
+    fn lex(input: &str) -> Vec<Element> {
+        let mut res = Vec::new();
+
+        let mut i = 0;
+        while i < input.len() {
+            match &input[i..i+1] {
+                "[" => { i += 1; res.push(Element::Open)},
+                "," => { i += 1; res.push(Element::Comma)},
+                "]" => { i += 1; res.push(Element::Close)},
+                _ => res.push(Element::Value(Pair::lex_value(&mut i, input))),
+            }
+        }
+
+        res
+    }
+    
+    fn lex_value(i: &mut usize, input: &str) -> i32 {
+        let start = *i;
+        while &input[*i..*i+1] != "," && &input[*i..*i+1] != "]" {
+            *i += 1;
+        }
+    
+        let sub = &input[start..*i];
+        sub.parse().unwrap()
+    }
+
+    fn add(lhs: &Pair, rhs: &Pair) -> Pair {
+        let sum = Pair {
+            lhs: Term::Pair(Box::new(lhs.clone())),
+            rhs: Term::Pair(Box::new(rhs.clone())),
+        };
+    
+        sum.reduce()
+    }
+    
+    fn reduce(&self) -> Self {
+        let mut elements = Vec::new();
+        self.append_to(&mut elements);
+
+        print!("Starting with: ");
+        Pair::print_elements(&elements);
+        loop {
+            let (res, reduced) = Pair::try_explode(&elements);
+            if reduced {
+                elements = res;
+                print!("Exploded to:   ");
+                Pair::print_elements(&elements);
+            } else {
+                let (res, reduced) = Pair::try_split(&elements);
+                if reduced {
+                    elements = res;
+                    print!("Split to:      ");
+                    Pair::print_elements(&elements);                    
+                } else {
+                    break;
+                }
+            }
+        }
+    
+        let mut i = 0;
+        Pair::parse(&mut i, &elements)
+    }
+
+    fn try_explode(elements: &Vec<Element>) -> (Vec<Element>, bool) {
+        let mut reduced = false;
+        let mut opens = 0;
+        let mut copy = Vec::new();
+        let mut pending = None;
+        copy.reserve(elements.len());
+        let mut i = 0;
+        while i < elements.len() {
+            if reduced {
+                if let Some(rhs) = pending {
+                    if let Element::Value(value) = elements[i] {
+                        pending = None;
+                        copy.push(Element::Value(rhs + value));
+                    } else {
+                        copy.push(elements[i].clone());
+                    }
+                } else {
+                    copy.push(elements[i].clone());
+                }
+                i += 1;
+            } else {
+                match &elements[i] {
+                    Element::Open => {
+                        opens += 1;
+                        if opens == 5 {
+                            if let Element::Value(lhs) = elements[i+1] {
+                                if let Element::Value(rhs) = elements[i+3] {
+                                    for x in (0..copy.len()).rev() {
+                                        if let Element::Value(value) = copy[x] {
+                                            copy[x] = Element::Value(value + lhs);
+                                            break;
+                                        }
+                                    }
+
+                                    pending = Some(rhs);
+                                    copy.push(Element::Value(0));
+
+                                    // skip past open, value, comma, value, close
+                                    i += 5;
+                                    reduced = true;
+                                } else {
+                                    panic!("Expected value, got {:?}", elements[i+3]);
+                                }
+                            } else {
+                                panic!("Expected value, got {:?}", elements[i+1]);
+                            }
+
+                        } else {
+                            copy.push(elements[i].clone());
+                            i += 1;
+                        }
+                    },
+                    Element::Close => {
+                        opens -= 1;
+                        copy.push(elements[i].clone());
+                        i += 1;
+                    },
+                    Element::Value(_) => {
+                        copy.push(elements[i].clone());
+                        i += 1;
+                    },
+                    Element::Comma => {
+                        copy.push(elements[i].clone());
+                        i += 1;
+                    },
+                }
+            }
+        }
+
+        (copy, reduced)
+    }
+
+    fn try_split(elements: &Vec<Element>) -> (Vec<Element>, bool) {
+        let mut reduced = false;
+        let mut copy = Vec::new();
+        copy.reserve(elements.len());
+        let mut i = 0;
+        while i < elements.len() {
+            if reduced {
+                copy.push(elements[i].clone());
+                i += 1;
+            } else {
+                match &elements[i] {
+                    Element::Value(v) => {
+                        if *v < 10 {
+                            copy.push(elements[i].clone());
+                        } else {
+                            copy.push(Element::Open);
+                            copy.push(Element::Value(*v / 2));
+                            copy.push(Element::Comma);
+                            copy.push(Element::Value((*v + 1)/ 2));
+                            copy.push(Element::Close);
+                            reduced = true;
+                        }
+                        i += 1;
+                    },
+                    Element::Open => {
+                        copy.push(elements[i].clone());
+                        i += 1;
+                    },
+                    Element::Close => {
+                        copy.push(elements[i].clone());
+                        i += 1;
+                    },
+                    Element::Comma => {
+                        copy.push(elements[i].clone());
+                        i += 1;
+                    },
+                }
+            }
+        }
+
+        (copy, reduced)
+    }
+
+    fn print_elements(elements: &Vec<Element>) {
+        for e in elements {
+            match e {
+                Element::Open => print!("["),
+                Element::Comma => print!(","),
+                Element::Close => print!("]"),
+                Element::Value(value) => print!("{}", value),
+            }
+        }
+        println!();
+    }
+
+    fn append_to(&self, vec: &mut Vec<Element>) {
+        vec.push(Element::Open);
+        self.lhs.append_to(vec);
+        vec.push(Element::Comma);
+        self.rhs.append_to(vec);
+        vec.push(Element::Close);
+    }
+    
     fn print(&self) {
         print!("[");
         self.lhs.print();
@@ -158,4 +317,12 @@ impl Pair {
     fn magnitude(&self) -> i32 {
         3 * self.lhs.magnitude() + 2 * self.rhs.magnitude()
     }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum Element {
+    Open,
+    Comma,
+    Close,
+    Value(i32),
 }
