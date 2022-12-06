43762ea3f569b001851aaaadcee2951c97814a31
Kevin Pilch <me@pilchie.com>
2022-12-06 16:25:56 +0000

    2022-12-06 - Part 2

--- 2022/Day6/main.go
+++ 2022/Day6/main.go
@@ -20,7 +20,7 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		for i := 3; i < len(line); i++ {
-			result = check_four(line, i)
+			result = check_n(line, i, 14)
 			if result > 0 {
 				fmt.Printf("Found start-of-packet at %d\n", result)
 				break
@@ -33,10 +33,10 @@ func main() {
 	}
 }
 
-func check_four(line string, index int) int {
+func check_n(line string, index int, n int) int {
 	set := make(map[byte]bool)
 	set[line[index]] = true
-	for j := 1; j <= 3; j++ {
+	for j := 1; j <= n-1; j++ {
 		if _, contains := set[line[index-j]]; !contains {
 			set[line[index-j]] = true
 		} else {
