36a7bc7fc3ef06d2e18e038ff30bbca74c50a5d6
Kevin Pilch <me@pilchie.com>
2022-12-15 21:26:19 +0000

    2022-12-15 - Part 1 - Simpler with manhattan distance

--- 2022/Day15/main.go
+++ 2022/Day15/main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -17,7 +16,7 @@ func main() {
 	}
 	defer f.Close()
 
-	locations := map[Point]float64{}
+	locations := map[Point]int{}
 	beacons := []Point{}
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -29,9 +28,9 @@ func main() {
 		by, _ := strconv.Atoi(parts[9][2:])
 		beacons = append(beacons, Point{x: bx, y: by})
 
-		dx := bx - sx
-		dy := by - sy
-		dist := math.Sqrt(float64(dx*dx + dy*dy))
+		dx := intabs(bx - sx)
+		dy := intabs(by - sy)
+		dist := dx + dy
 
 		locations[Point{x: sx, y: sy}] = dist
 	}
@@ -79,7 +78,7 @@ type Range struct {
 	max int
 }
 
-func findIntersections(locations map[Point]float64, y_value int) []Range {
+func findIntersections(locations map[Point]int, y_value int) []Range {
 	res := []Range{}
 	for p := range locations {
 		r := findIntersection(p, locations[p], y_value)
@@ -90,31 +89,25 @@ func findIntersections(locations map[Point]float64, y_value int) []Range {
 	return res
 }
 
-// From https://cp-algorithms.com/geometry/circle-line-intersection.html
-func findIntersection(p Point, r float64, y_value int) *Range {
-	a := 0.0
-	b := 1.0
-	c := float64(-y_value + p.y)
-	x0 := -a * c / (a*a + b*b)
-	//y0 := -b * c / (a*a + b*b)
-
-	fmt.Printf("Finding intersection of circle at (%2d,%2d) radius: %5.2f", p.x, p.y, r)
-	if c*c > r*r*(a*a+b*b+0.001) {
-		fmt.Println(" - no intersection")
+func findIntersection(p Point, distance int, y_value int) *Range {
+	fmt.Printf("Finding the intersection of (%d,%d) with range %d, and line at y=%d",
+		p.x, p.y, distance, y_value)
+	y_dist := intabs(p.y - y_value)
+	if y_dist > distance {
+		fmt.Printf(" - y_dist is %d, so no intersection\n", y_dist)
 		return nil
-	} else if math.Abs(c*c-r*r*(a*a+b*b)) < 0.001 {
-		res := Range{min: int(math.Round(x0)) + p.x, max: int(math.Round(x0)) + p.x}
-		fmt.Printf(" - single result at %d\n", res.min)
-		return &res
+	}
+
+	min := p.x - (distance - y_dist)
+	max := p.x + (distance - y_dist)
+	fmt.Printf(" - y_dist is %d, so intersection from %d to %d\n", y_dist, min, max)
+	return &Range{min, max}
+}
+
+func intabs(i int) int {
+	if i < 0 {
+		return -1 * i
 	} else {
-		d := r*r - c*c/(a*a+b*b)
-		mult := math.Sqrt(d / (a*a + b*b))
-		ax := x0 + b*mult
-		bx := x0 - b*mult
-		//ay = y0 - a*mult
-		//by = y0 + a*mult
-		res := Range{min: int(math.Round(math.Min(ax, bx))) + p.x, max: int(math.Round(math.Max(ax, bx))) + p.x}
-		fmt.Printf(" - result from %d to %d\n", res.min, res.max)
-		return &res
+		return i
 	}
 }
