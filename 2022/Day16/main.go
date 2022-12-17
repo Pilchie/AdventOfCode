664df05d6b35cef0f3f625d8acc571da97d85ee5
Kevin Pilch <me@pilchie.com>
2022-12-17 03:30:14 +0000

    2022-12-16 - Part one
    Heavily inspired by https://www.reddit.com/r/adventofcode/comments/zn6k1l/comment/j0fvvlj/.
    
    Took way too long!

--- 2022/Day16/main.go
+++ 2022/Day16/main.go
@@ -27,12 +27,18 @@ func main() {
 		valve.print()
 	}
 
-	open := map[string]bool{}
-	position := "AA"
-	step := 0
-	limit := 30
-	pressure := step_recursive(position, valves, open, step, limit, "AA")
-	fmt.Printf("Released %d pressure\n", pressure)
+	openValveMask := make(map[string]uint16)
+	bitPosition := 0
+	for k, v := range valves {
+		if v.flowRate > 0 {
+			fmt.Printf("Setting mask for %s to %04x\n", k, 1<<bitPosition)
+			openValveMask[k] = 1 << bitPosition
+			bitPosition++
+		}
+	}
+
+	pressure, path := part1(valves, openValveMask, 30)
+	fmt.Printf("Released %d pressure via %s\n", pressure, path)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -67,64 +73,114 @@ func parseValve(input string) Valve {
 	return Valve{name, flowRate, destinations}
 }
 
-func current_flow(open map[string]bool, valves map[string]Valve) int {
-	pressure := 0
-	for o := range open {
-		pressure += valves[o].flowRate
-	}
-	return pressure
+type State struct {
+	openState   uint16
+	position    string
+	minute      int
+	currentFlow int
+	totalFlow   int
+	path        string
 }
 
-func step_recursive(position string, valves map[string]Valve, open map[string]bool, step int, limit int, path string) int {
-	step++
-	if step > limit {
-		return 0
+func count(m map[string]map[uint16]int) int {
+	res := 0
+	for _, v := range m {
+		res += len(v)
 	}
+	return res
+}
 
-	current := current_flow(open, valves)
+func part1(valves map[string]Valve, openValveMask map[string]uint16, limit int) (int, string) {
+	largestFlow := math.MinInt
+	largestPath := ""
 
-	// All valves are open, do nothing
-	if len(open) == len(valves) {
-		return current + step_recursive(position, valves, open, step, limit, path+position)
+	allTrue := uint16(0xFFFF)
+	var initialOpens uint16
+	for i := len(openValveMask); i < 16; i++ {
+		initialOpens = initialOpens | (1 << i)
 	}
+	fmt.Printf("Initial Opens is %x\n", initialOpens)
 
-	// Open the current valve
-	valve := valves[position]
-	max_children := math.MinInt
-	if valve.flowRate > 0 {
-		if v, f := open[position]; !f || !v {
-			// clone open, add current
-			cloned := clone(open)
-			cloned[position] = true
-			child := step_recursive(position, valves, cloned, step, limit, path+"o")
-			if child > max_children {
-				max_children = child
+	searchSpace := []State{{position: "AA", minute: 0, openState: initialOpens, path: "AA"}}
+
+	seen := make(map[string]map[uint16]int)
+	prevMinute := 0
+
+nextState:
+	for {
+		searchSpaceSize := len(searchSpace)
+		if searchSpaceSize == 0 {
+			break
+		}
+
+		currentState := searchSpace[0]
+		searchSpace = searchSpace[1:]
+
+		if currentState.minute != prevMinute {
+			fmt.Printf("Processing minute %d, with %d items, seen %d, currentVal: %d\n", currentState.minute, searchSpaceSize, count(seen), currentState.totalFlow)
+			prevMinute = currentState.minute
+		}
+
+		if currentState.minute >= limit {
+			if currentState.totalFlow > largestFlow {
+				largestFlow = currentState.totalFlow
+				largestPath = currentState.path
+			}
+			continue nextState
+		}
+
+		if opens, ok := seen[currentState.position]; !ok {
+			seen[currentState.position] = make(map[uint16]int)
+		} else {
+			for key, val := range opens {
+				if val > currentState.totalFlow {
+					// If we've seen the same *or more* valves open, we don't need to continue down this path
+					if key&currentState.openState == currentState.openState {
+						continue nextState
+					}
+				}
 			}
 		}
-	} else {
-		// Mark this open, so we don't look at it again
-		open[position] = true
-	}
+		seen[currentState.position][currentState.openState] = currentState.totalFlow
 
-	// or, travel through one of the tunnels
-	for d := range valve.destinations {
-		child := step_recursive(valve.destinations[d], valves, open, step, limit, path+valve.destinations[d])
-		if child > max_children {
-			max_children = child
+		if currentState.openState == allTrue {
+			// Don't need to move, everything is open
+			searchSpace = append(searchSpace, State{
+				position:    currentState.position,
+				openState:   currentState.openState,
+				minute:      currentState.minute + 1,
+				currentFlow: currentState.currentFlow,
+				totalFlow:   currentState.totalFlow + currentState.currentFlow,
+				path:        currentState.path + "->" + currentState.position,
+			})
+		} else {
+			// If we're at a closed valve, we could open it.
+			open := currentState.openState&openValveMask[currentState.position] != 0
+			if !open && valves[currentState.position].flowRate > 0 {
+				openState := currentState.openState
+				openState = openState | openValveMask[currentState.position]
+				searchSpace = append(searchSpace, State{
+					position:    currentState.position,
+					openState:   openState,
+					minute:      currentState.minute + 1,
+					currentFlow: currentState.currentFlow + valves[currentState.position].flowRate,
+					totalFlow:   currentState.totalFlow + currentState.currentFlow,
+					path:        currentState.path + "-o",
+				})
+			}
+
+			for _, v := range valves[currentState.position].destinations {
+				searchSpace = append(searchSpace, State{
+					position:    v,
+					openState:   currentState.openState,
+					minute:      currentState.minute + 1,
+					currentFlow: currentState.currentFlow,
+					totalFlow:   currentState.totalFlow + currentState.currentFlow,
+					path:        currentState.path + "->" + v,
+				})
+			}
 		}
 	}
 
-	res := current + max_children
-	if step < 15 {
-		fmt.Printf("%s - %d\n", path, res)
-	}
-	return res
-}
-
-func clone(m map[string]bool) map[string]bool {
-	res := map[string]bool{}
-	for k := range m {
-		res[k] = m[k]
-	}
-	return res
+	return largestFlow, largestPath
 }
