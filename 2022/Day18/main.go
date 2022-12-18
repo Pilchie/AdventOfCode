47e01678f8c78b0c3bfee24b9db17e4147456a58
Kevin Pilch <me@pilchie.com>
2022-12-18 22:39:22 +0000

    2022-12-18 - Part 2

--- 2022/Day18/main.go
+++ 2022/Day18/main.go
@@ -4,10 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -29,16 +31,21 @@ func main() {
 		cubes = append(cubes, cube)
 	}
 
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	part1(cubes)
+	part2(cubes)
+}
+
+func part1(cubes [][3]int) {
 	area := len(cubes) * 6
 	area -= adjacentFaces(cubes, 0)
 	area -= adjacentFaces(cubes, 1)
 	area -= adjacentFaces(cubes, 2)
 
 	fmt.Printf("The area is %d\n", area)
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
 }
 
 func adjacentFaces(cubes [][3]int, axis int) int {
@@ -68,4 +75,70 @@ func adjacentFaces(cubes [][3]int, axis int) int {
 		}
 	}
 	return adjacent * 2
+}
+
+func part2(cubes [][3]int) {
+	// Determine the bounding box, and, starting from one corner, enumerate all reachable cubes and count their faces.
+
+	startTime := time.Now()
+
+	mins := [3]int{math.MaxInt, math.MaxInt, math.MaxInt}
+	maxs := [3]int{math.MinInt, math.MinInt, math.MinInt}
+
+	cubePoints := make(map[[3]int]bool)
+	for _, p := range cubes {
+		cubePoints[p] = true
+		for ax := 0; ax < len(mins); ax++ {
+			if p[ax] < mins[ax] {
+				mins[ax] = p[ax]
+			}
+			if p[ax] > maxs[ax] {
+				maxs[ax] = p[ax]
+			}
+		}
+	}
+
+	// Start one past the minimums, in case there is a cube at minx,miny,minz
+	toExplore := [][3]int{{mins[0] - 1, mins[1] - 1, mins[2] - 1}}
+	seen := map[[3]int]bool{}
+	surfaceArea := 0
+
+	for len(toExplore) > 0 {
+		current := toExplore[0]
+		toExplore = toExplore[1:]
+
+		if _, ok := seen[current]; ok {
+			continue
+		}
+		seen[current] = true
+
+		for ax := 0; ax < len(mins); ax++ {
+			// explore negative direction
+			if current[ax] > mins[ax]-1 {
+				potential := current
+				potential[ax]--
+				if _, ok := cubePoints[potential]; ok {
+					// There is a cube there, so there is a unit of surface area between this cube and it
+					surfaceArea++
+				} else {
+					// No cube, so add it to the list to explore
+					toExplore = append(toExplore, potential)
+				}
+			}
+			// explore positive direction
+			if current[ax] <= maxs[ax]+1 {
+				potential := current
+				potential[ax]++
+				if _, ok := cubePoints[potential]; ok {
+					// There is a cube there, so there is a unit of surface area between this cube and it
+					surfaceArea++
+				} else {
+					// No cube, so add it to the list to explore
+					toExplore = append(toExplore, potential)
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Exterior surface area is %d (calculated in %v)\n", surfaceArea, time.Since(startTime))
 }
