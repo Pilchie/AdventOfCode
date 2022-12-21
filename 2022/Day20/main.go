37566a5d1ec433df13a4624e115e7b7af2558a5d
Kevin Pilch <me@pilchie.com>
2022-12-21 02:26:08 +0000

    2022-12-20 - Part 2

--- 2022/Day20/main.go
+++ 2022/Day20/main.go
@@ -77,6 +77,8 @@ func (cl *CircularList) print() {
 }
 
 func part1(numbers []int) {
+	fmt.Println("-----------------")
+	fmt.Println("Starting Part One")
 	res := CircularList{}
 	nodes := []*Node{}
 	for _, n := range numbers {
@@ -138,5 +140,78 @@ func part1(numbers []int) {
 }
 
 func part2(numbers []int) {
+	fmt.Println("-----------------")
+	fmt.Println("Starting Part Two")
+	res := CircularList{}
+	nodes := []*Node{}
+	for i, n := range numbers {
+		new := n * 811589153
+		numbers[i] = new % (len(numbers) - 1)
+		nodes = append(nodes, res.append(new))
+	}
 
+	fmt.Print("Initial ")
+	res.print()
+	fmt.Println()
+
+	for mix := 0; mix < 10; mix++ {
+		for i, changeby := range numbers {
+			cur := nodes[i]
+			if changeby > 0 {
+				changeby = changeby % len(numbers)
+				for i := 0; i < changeby; i++ {
+					p := cur.prev
+					n := cur.next
+					nn := n.next
+					p.next = n
+					n.prev = p
+					n.next = cur
+					cur.prev = n
+					cur.next = nn
+					nn.prev = cur
+				}
+			} else {
+				changeby = changeby % len(numbers)
+				for i := 0; i > changeby; i-- {
+					p := cur.prev
+					pp := p.prev
+					n := cur.next
+					pp.next = cur
+					cur.prev = pp
+					cur.next = p
+					p.prev = cur
+					p.next = n
+					n.prev = p
+				}
+			}
+		}
+
+		fmt.Printf("After mixing %d times: ", mix+1)
+		//res.print()
+		fmt.Println()
+	}
+
+	cur := res.head
+	afterZero := 0
+	started := false
+	sum := 0
+	for {
+		if cur.val == 0 {
+			started = true
+		}
+
+		if started {
+			if afterZero%1000 == 0 {
+				sum += cur.val
+				fmt.Printf("Val %d is %d\n", afterZero, cur.val)
+			}
+			if afterZero == 3000 {
+				break
+			}
+			afterZero++
+		}
+
+		cur = cur.next
+	}
+	fmt.Printf("The sum is %d\n", sum)
 }
