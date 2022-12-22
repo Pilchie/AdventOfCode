4cb80f8ac2a30bc220e30c7925eeb6643c73e179
Kevin Pilch <me@pilchie.com>
2022-12-22 01:17:41 +0000

    2022-12-19 - Part 2 WIP

--- 2022/Day19/main.go
+++ 2022/Day19/main.go
@@ -33,24 +33,24 @@ func main() {
 }
 
 type BluePrint struct {
-	id    byte
-	costs [4][3]byte
-	maxes [4]byte
+	id    uint8
+	costs [4][3]uint8
+	maxes [4]uint8
 }
 
-func (bp *BluePrint) maxResourceCost(resourceIndex int) byte {
+func (bp *BluePrint) maxResourceCost(resourceIndex int) uint8 {
 	return bp.maxes[resourceIndex]
 }
 
 func parseBlueBrint(input string) BluePrint {
 	parts := strings.Split(input, " ")
 	bp := BluePrint{
-		id: parseByte(strings.TrimRight(parts[1], ":")),
+		id: parseUInt8(strings.TrimRight(parts[1], ":")),
 		costs: [4][3]byte{
-			{parseByte(parts[6]), 0, 0},
-			{parseByte(parts[12]), 0, 0},
-			{parseByte(parts[18]), parseByte(parts[21]), 0},
-			{parseByte(parts[27]), 0, parseByte(parts[30])},
+			{parseUInt8(parts[6]), 0, 0},
+			{parseUInt8(parts[12]), 0, 0},
+			{parseUInt8(parts[18]), parseUInt8(parts[21]), 0},
+			{parseUInt8(parts[27]), 0, parseUInt8(parts[30])},
 		},
 	}
 
@@ -67,12 +67,12 @@ func parseBlueBrint(input string) BluePrint {
 	return bp
 }
 
-func parseByte(input string) byte {
+func parseUInt8(input string) uint8 {
 	val, err := strconv.Atoi(input)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	return byte(val)
+	return uint8(val)
 }
 
 func part1(bluePrints []BluePrint) {
@@ -109,24 +109,24 @@ func part2(bluePrints []BluePrint) {
 }
 
 type State struct {
-	minute    byte
-	opened    byte
-	materials [4]byte
-	robots    [4]byte
+	minute    uint8
+	opened    uint8
+	materials [4]int16
+	robots    [4]uint8
 	prev      *State
 }
 
 type Cache struct {
-	materials [4]byte
-	robots    [4]byte
+	materials [4]int16
+	robots    [4]uint8
 }
 
-func maxGeodes(bluePrint BluePrint, limit byte) byte {
-	best := State{minute: 1, opened: 0, robots: [4]byte{1, 0, 0, 0}}
+func maxGeodes(bluePrint BluePrint, limit uint8) uint8 {
+	best := State{minute: 1, opened: 0, robots: [4]uint8{1, 0, 0, 0}}
 
 	searchSpace := Queue{}
 	searchSpace.push(best)
-	seen := map[Cache]byte{}
+	seen := map[Cache]uint8{}
 
 	for !searchSpace.isEmpty() {
 		current := searchSpace.pop()
@@ -149,44 +149,52 @@ func maxGeodes(bluePrint BluePrint, limit byte) byte {
 		}
 
 		// If we're in the last minute, there is no point building another robot, since it won't have time do anything.
-		//if current.minute < limit {
-		// Pick which robot to build next
-		for robotIdx, robotCosts := range bluePrint.costs {
-			if robotIdx < len(bluePrint.costs)-1 {
-				timeLeft := limit - current.minute
-				// If we already have enough of robotIdx to build any other robot every minute, don't bother building more.
-				if current.robots[robotIdx]*timeLeft+current.materials[robotIdx] >= bluePrint.maxResourceCost(robotIdx)*timeLeft {
-					continue
-				}
-			}
-			canBuild := true
-			timeToBuild := byte(0)
-			for costIdx, cost := range robotCosts {
-				if cost != 0 && current.robots[costIdx] == 0 {
-					canBuild = false
-					break
-				} else {
-					resourcesNeeded := cost - current.materials[costIdx]
-					timeResource := byte(math.Ceil(float64(resourcesNeeded) / float64(current.robots[costIdx])))
-					if timeResource > timeToBuild {
-						timeToBuild = timeResource
+		if current.minute < limit {
+			// Pick which robot to build next
+			for robotIdx := len(bluePrint.costs) - 1; robotIdx >= 0; robotIdx-- {
+				if robotIdx < len(bluePrint.costs)-1 {
+					robots := current.robots[robotIdx]
+					// timeLeft := limit - current.minute
+					// materials := current.materials[robotIdx]
+					// If we already have enough of robotIdx to build any other robot every minute, don't bother building more.
+					// if robots*timeLeft+materials >= bluePrint.maxResourceCost(robotIdx)*timeLeft {
+					// 	continue
+					// }
+					if robots >= bluePrint.maxResourceCost(robotIdx) {
+						continue
 					}
 				}
-			}
-
-			if canBuild && current.minute+timeToBuild < limit-1 {
-				n := next(current, timeToBuild+1)
-				n.robots[robotIdx]++
+				canBuild := true
+				timeToBuild := uint8(0)
+				robotCosts := bluePrint.costs[robotIdx]
 				for costIdx, cost := range robotCosts {
-					n.materials[costIdx] -= cost
-					if n.materials[costIdx] > 200 {
-						fmt.Println("Underflowed resources")
+					if cost != 0 && current.robots[costIdx] == 0 {
+						canBuild = false
+						break
+					} else {
+						if int16(cost) > current.materials[costIdx] {
+							resourcesNeeded := int16(cost) - current.materials[costIdx]
+							timeResource := uint8(math.Ceil(float64(resourcesNeeded) / float64(current.robots[costIdx])))
+							if timeResource > timeToBuild {
+								timeToBuild = timeResource
+							}
+						}
 					}
 				}
-				searchSpace.push(n)
+
+				if canBuild && current.minute+timeToBuild < limit {
+					n := next(current, timeToBuild+1)
+					n.robots[robotIdx]++
+					for costIdx, cost := range robotCosts {
+						n.materials[costIdx] -= int16(cost)
+						if n.materials[costIdx] < 0 {
+							fmt.Println("Underflowed resources")
+						}
+					}
+					searchSpace.push(n)
+				}
 			}
 		}
-		//}
 
 		// There is always an option where we don't build anything.
 		searchSpace.push(next(current, 1))
@@ -205,13 +213,13 @@ func maxGeodes(bluePrint BluePrint, limit byte) byte {
 	return best.opened
 }
 
-func next(current State, minutes byte) State {
+func next(current State, minutes uint8) State {
 	next := current
 	next.prev = &current
 	next.minute += minutes
 	next.opened = current.opened + minutes*current.robots[3]
 	for i := 0; i < len(current.robots); i++ {
-		next.materials[i] += minutes * current.robots[i]
+		next.materials[i] += int16(minutes * current.robots[i])
 	}
 	return next
 }
