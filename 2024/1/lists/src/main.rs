07e7a441ff8ce541c9282789d7412c55e74c44cc
Kevin Pilch <me@pilchie.com>
2024-12-01 21:12:58 +0000

    2024 - Day 1 - Part 1

--- 2024/1/lists/src/main.rs
+++ 2024/1/lists/src/main.rs
@@ -1,3 +1,31 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
+
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in contents.lines() {
+        let mut parts = line.split("   ");
+        let left_str = parts.next().unwrap();
+        let right_str = parts.next().unwrap();
+
+        let left_part = left_str.parse::<i32>().unwrap();
+        let right_part = right_str.parse::<i32>().unwrap();
+        left.push(left_part);
+        right.push(right_part);
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut accum = 0;
+    for i in 0..std::cmp::min(left.len(), right.len()) {
+        accum += (left[i] - right[i]).abs();
+    }
+
+    println!("{}", accum);
 }
