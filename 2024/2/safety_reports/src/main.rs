36f29221111d271d461f40629b8fe5a45b26611c
Kevin Pilch <me@pilchie.com>
2024-12-02 18:57:51 +0000

    2024 - Day 2 - Part 1 - initial attempt

--- 2024/2/safety_reports/src/main.rs
+++ 2024/2/safety_reports/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::{BufRead, BufReader, Error};
+
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+
+    let reader = BufReader::new(std::fs::File::open(&args[1])?);
+
+    let mut safe = 0;
+    for line in reader.lines() {
+        if is_safe(&line?) {
+            safe += 1;
+        }
+    }
+
+    println!("{} safe reports", safe);
+
+    Ok(())
+}
+
+fn is_safe(report: &str) -> bool {
+    let mut ascending: Option<bool> = None;
+    let mut prev: Option<u32> = None;
+    for val_str in report.split_whitespace() {
+        let val = val_str.parse::<u32>().unwrap();
+        if let Some(p) = prev {
+            match ascending {
+                Some(true) => {
+                    if val <= p || val - p > 3 {
+                        return false;
+                    }
+                }
+                Some(false) => {
+                    if val >= p || p - val > 3 {
+                        return false;
+                    }
+                }
+                None => {
+                    ascending = Some(val > p);
+                }
+            }
+        }
+        prev = Some(val);
+    }
+    true
 }
