f035e14f456cb7433f02eba4d902cb7dcdd8504b
Kevin Pilch <me@pilchie.com>
2024-12-02 19:58:51 +0000

    2024 - Day 2 - Part 2
    Brute force it by trying all removals.

--- 2024/2/safety_reports/src/main.rs
+++ 2024/2/safety_reports/src/main.rs
@@ -19,36 +19,50 @@ fn main() -> Result<(), Error> {
 }
 
 fn is_safe(report: &str, allow_dampening: bool) -> bool {
+    let values: Vec<_> = report
+        .split_whitespace()
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
+
+    if allow_dampening {
+        for i in 0..values.len() {
+            let mut values = values.clone();
+            values.remove(i);
+
+            if is_safe_values(&values) {
+                return true;
+            }
+        }
+    }
+
+    is_safe_values(&values)
+}
+
+fn is_safe_values(values: &[u32]) -> bool {
     let mut ascending: Option<bool> = None;
     let mut prev: Option<u32> = None;
-    let mut seen_large = false;
-    for val_str in report.split_whitespace() {
-        let val = val_str.parse::<u32>().unwrap();
+
+    for val in values {
         if let Some(p) = prev {
             if ascending.is_none() {
-                ascending = Some(val > p);
+                ascending = Some(*val > p);
             }
             match ascending {
                 Some(true) => {
-                    if val <= p || val - p > 3 {
-                        if seen_large || !allow_dampening {
-                            return false;
-                        }
-                        seen_large = true;
+                    if *val <= p || val - p > 3 {
+                        return false;
                     }
                 }
                 Some(false) => {
-                    if val >= p || p - val > 3 {
-                        if seen_large || !allow_dampening {
-                            return false;
-                        }
-                        seen_large = true;
+                    if *val >= p || p - val > 3 {
+                        return false;
                     }
                 }
                 None => unreachable!(),
             }
         }
-        prev = Some(val);
+        prev = Some(*val);
     }
+
     true
 }
