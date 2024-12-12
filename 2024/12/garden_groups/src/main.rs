ff0fef2d9deb09476496e6aa5df8daf5bb6712b8
Kevin Pilch <me@pilchie.com>
2024-12-12 16:48:32 +0000

    2024 - Day 12 - WIP

--- 2024/12/garden_groups/src/main.rs
+++ 2024/12/garden_groups/src/main.rs
@@ -1,3 +1,38 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
+
+    let regions = Region::parse_map(&contents);
+    let cost: usize = regions.iter().map(|r| r.cost()).sum();
+    println!("The total cost is {}", cost);
+}
+
+struct Region {
+    plots: Vec<Point>
+}
+
+impl Region {
+    fn parse_map(_input: &str) -> Vec<Self> {
+        Vec::new()
+    }
+
+    fn cost(&self) -> usize {
+        self.area() * self.perimeter()
+    }
+
+    fn area(&self) -> usize {
+        self.plots.len()
+    }
+
+    fn perimeter(&self) -> usize {
+        0
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Point {
+    x: usize,
+    y: usize,
 }
