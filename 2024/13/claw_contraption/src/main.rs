a4a1a8df2283aada2c380840991e0e155cbcd540
Kevin Pilch <me@pilchie.com>
2024-12-14 04:58:47 +0000

    2024 - Day 13 - Part 2 - WIP

--- 2024/13/claw_contraption/src/main.rs
+++ 2024/13/claw_contraption/src/main.rs
@@ -6,46 +6,48 @@ fn main() {
     let machines = Machine::parse_list(&contents);
 
     let mut total_tokens = 0;
+    let mut count = 0;
     for m in machines {
         if let Some(tokens) = m.minimum_tokens() {
             total_tokens += tokens;
+            count += 1;
         }
     }
 
     println!(
-        "It would take {} total tokens to win all prizes.",
-        total_tokens
+        "It would take {} total tokens to win all prizes, from {} games.",
+        total_tokens, count
     );
 }
 
 struct Button {
-    x: usize,
-    y: usize,
-    cost: usize,
+    x: i64,
+    y: i64,
+    cost: i64,
 }
 
 impl Button {
-    fn parse(line: &str, cost: usize) -> Self {
+    fn parse(line: &str, cost: i64) -> Self {
         let (_, a) = line.split_once(": ").unwrap();
         let (xstr, ystr) = a.split_once(", ").unwrap();
-        let x = xstr[2..].parse::<usize>().unwrap();
-        let y = ystr[2..].parse::<usize>().unwrap();
+        let x = xstr[2..].parse::<i64>().unwrap();
+        let y = ystr[2..].parse::<i64>().unwrap();
 
         Self { x, y, cost }
     }
 }
 
 struct Prize {
-    x: usize,
-    y: usize,
+    x: i64,
+    y: i64,
 }
 
 impl Prize {
     fn parse(line: &str) -> Self {
         let (_, rest) = line.split_once(": ").unwrap();
         let (xstr, ystr) = rest.split_once(", ").unwrap();
-        let x = xstr[2..].parse::<usize>().unwrap();
-        let y = ystr[2..].parse::<usize>().unwrap();
+        let x = /*10000000000000 +*/ xstr[2..].parse::<i64>().unwrap();
+        let y = /*10000000000000 +*/ ystr[2..].parse::<i64>().unwrap();
 
         Self { x, y }
     }
@@ -76,21 +78,41 @@ impl Machine {
         res
     }
 
-    fn minimum_tokens(&self) -> Option<usize> {
-        let mut solutions = Vec::new();
-        for a in 0..101 {
-            for b in 0..101 {
-                let xloc = a * self.button_a.x + b * self.button_b.x; 
-                let yloc = a * self.button_a.y + b * self.button_b.y;
-                if xloc == self.prize.x && yloc == self.prize.y {
-                    let tokens = a * self.button_a.cost + b * self.button_b.cost;
-                    solutions.push(tokens);
-                } else if xloc > self.prize.x || yloc > self.prize.y { 
-                    break;
-                }
-            }
+    fn minimum_tokens(&self) -> Option<i64> {
+        if self.button_a.y * self.button_b.x == self.button_a.x * self.button_b.y {
+            println!(
+                "No solution (div0) for machine with prize at ({},{})",
+                self.prize.x, self.prize.y
+            );
+            return None;
         }
-        solutions.sort();
-        solutions.first().copied()
+
+        let a = (self.prize.y * self.button_b.x - self.prize.x * self.button_b.y)
+            / (self.button_a.y * self.button_b.x - self.button_a.x * self.button_b.y);
+
+        let b = (self.prize.x - a * self.button_a.x) / self.button_b.x;
+
+        if a < 0 || b < 0 {
+            // println!(
+            //     "No solution (neg)  for machine with prize at ({},{}) (a/b) are: ({},{})",
+            //     self.prize.x, self.prize.y, a, b
+            // );
+            return None;
+        }
+
+        if a >= 100 || b >= 100 {
+            // println!(
+            //     "No solution (too far) for machine with prize at ({},{}) (a/b) are: ({},{})",
+            //     self.prize.x, self.prize.y, a, b
+            // );
+            return None;
+        }
+
+        let tokens = a * self.button_a.cost + b * self.button_b.cost;
+        println!(
+            "Solution for machine with prize at ({},{}) costs {}, (a/b) is ({}/{})",
+            self.prize.x, self.prize.y, tokens, a, b
+        );
+        Some(tokens)
     }
 }
