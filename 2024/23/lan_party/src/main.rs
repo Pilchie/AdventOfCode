5d5b65481956273dc4e17102f46f7aefa1fef27e
Kevin Pilch <me@pilchie.com>
2024-12-23 21:16:41 +0000

    2024 - Day 23 - Part 2

--- 2024/23/lan_party/src/main.rs
+++ 2024/23/lan_party/src/main.rs
@@ -12,7 +12,13 @@ fn main() {
         .iter()
         .filter(|c|c.iter().any(|v| v.starts_with('t')))
         .count();
-    println!("There are {} connected components that contain a computer with a t", triangles_with_ts);
+    println!("There are {} triangles that contain a computer with a t", triangles_with_ts);
+
+    let mut maximal_cliques = graph.find_maximal_cliques();
+    maximal_cliques.sort_by(|v1, v2| v1.len().cmp(&v2.len()));
+    let mut lan_party = maximal_cliques.last().unwrap().clone();
+    lan_party.sort();
+    println!("The password is {}", lan_party.join(","));
 }
 
 struct Graph<'a> {
@@ -47,6 +53,60 @@ impl<'a> Graph<'a> {
                 }
             }
         }
+        res
+    }
+
+    // Use the BronKerbosch algorithm
+    // from https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+    fn find_maximal_cliques(&self) -> Vec<Vec<&'a str>> {
+        let mut res = Vec::new();
+        let p = self.vertices.clone();
+        let r = HashSet::new();
+        let x = HashSet::new();
+
+        self.find_maximal_cliques_recursive(&r, &p, &x, &mut res);
+        res
+    }
+
+     fn find_maximal_cliques_recursive(&self, r: &HashSet<&'a str>, p: &HashSet<&'a str>, x: &HashSet<&'a str>, res: &mut Vec<Vec<&'a str>>) {
+        if p.is_empty() && x.is_empty() {
+            let mut v = Vec::new();
+            for n in r {
+                v.push(*n);
+            }
+            res.push(v);
+        } else {
+            let mut p = p.clone();
+            let mut x = x.clone();
+            let mut removed = HashSet::new();
+            for v in p.clone() {
+                if removed.contains(v) {
+                    continue;
+                }
+                let mut new_r = r.clone();
+                new_r.insert(v);
+                let neighbors_of_v = self.find_neighbors(v);
+                let new_p = p.intersection(&neighbors_of_v).map(|i| *i).collect();
+                let new_x = p.intersection(&neighbors_of_v).map(|i| *i).collect();
+
+                self.find_maximal_cliques_recursive(&new_r, &new_p, &new_x, res);
+                p.remove(v);
+                removed.insert(v);
+                x.insert(v);
+            }
+        }
+    }
+
+    fn find_neighbors(&self, vertex: &'a str) -> HashSet<&'a str> {
+        let mut res = HashSet::new();
+        for (v1, v2) in &self.edges {
+            if vertex == *v1 {
+                res.insert(*v2);
+            } else if vertex == *v2 {
+                res.insert(*v1);
+            }
+        }
+
         res
     }
 
