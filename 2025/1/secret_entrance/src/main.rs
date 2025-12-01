de756483f37a1bfc6cc3738e3bdaf60007c2564e
Kevin Pilch <me@pilchie.com>
2025-12-01 22:37:39 +0000

    2025-12-01 - Part 1

--- 2025/1/secret_entrance/src/main.rs
+++ 2025/1/secret_entrance/src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
+
+    let mut pos = 50;
+    let mut sum = 0;
+    for line in contents.lines() {
+        let num: i32 = line[1..].parse().unwrap();
+        pos += match &line[0..1] {
+            "L" => -1 * num,
+            "R" => num,
+            _ => panic!("Unknown direction"),
+        };
+
+        pos = pos % 100;
+        if pos < 0 {
+            pos += 100;
+        }
+
+        if pos == 0 {
+            sum += 1;
+        }
+        
+    }
+
+    println!("The correct password is: {}", sum);
 }
