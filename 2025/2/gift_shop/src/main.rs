9451c47942524fa8342d05e013f388be6c139759
Kevin Pilch <me@pilchie.com>
2025-12-02 21:17:25 +0000

    2025-12-02 - Init

---
+++ 2025/2/gift_shop/src/main.rs
@@ -0,0 +1,13 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
+    part_one(&contents);
+}
+
+fn part_one(contents: &str) {
+    let mut sum = 0;
+    println!("Part 1: The sum of all of the invalid ids is: {}", sum);
+}
