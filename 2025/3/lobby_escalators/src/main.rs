0710e17f60b1477134ddf891d6f2109aeec72a35
Kevin Pilch <me@pilchie.com>
2025-12-03 16:12:41 +0000

    2025-12-03 - Init

---
+++ 2025/3/lobby_escalators/src/main.rs
@@ -0,0 +1,12 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
+    part_one(&contents);
+}
+fn part_one(contents: &str) {
+    let mut sum = 0;
+    println!("Part 1: The total output joltage is: {}", sum);
+}
