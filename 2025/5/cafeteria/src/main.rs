ed202bc20714d58cb0931198251361509aecb34b
Kevin Pilch <me@pilchie.com>
2025-12-05 19:34:30 +0000

    2025-12-05 - Part 1

--- 2025/5/cafeteria/src/main.rs
+++ 2025/5/cafeteria/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, env, fs};
+use std::{env, fs, ops::RangeInclusive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,7 +9,30 @@ fn main() {
 }
 
 fn part_one(contents: &str) {
-    println!("Part 1:");
+    let mut fresh = 0;
+
+    let mut ranges = Vec::new();
+    for line in contents.lines() {
+        if line.trim().is_empty() {
+            break;
+        }
+        let (l, r) = line.split_once('-').unwrap();
+        let l = l.parse::<u64>().unwrap();
+        let r = r.parse::<u64>().unwrap();
+        ranges.push(RangeInclusive::new(l, r));
+    }
+    ranges.sort_by_key(|r| *r.start());
+
+    for line in contents.lines().skip(ranges.len() + 1) {
+        let ingredient = line.parse::<u64>().unwrap();
+        for range in &ranges {
+            if range.contains(&ingredient) {
+                fresh += 1;
+                break;
+            }
+        }
+    }
+    println!("Part 1: {} of the available ingredient IDs are fresh", fresh);
 }
 
 fn part_two(contents: &str) {
