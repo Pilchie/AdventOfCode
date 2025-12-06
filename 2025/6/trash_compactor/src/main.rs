0db0c4aae07e0fca72fe6f150a6e964b2404710e
Kevin Pilch <me@pilchie.com>
2025-12-06 22:29:38 +0000

    2025-12-06 - Part 1

--- 2025/6/trash_compactor/src/main.rs
+++ 2025/6/trash_compactor/src/main.rs
@@ -9,5 +9,30 @@ fn main() {
 
 fn part_one(contents: &str) {
     let mut sum = 0;
+    let mut rows = Vec::new();
+    for line in contents.lines() {
+        let row: Vec<_> = line.split_ascii_whitespace().collect();
+        rows.push(row);
+    }
+
+    for i in 0..rows[0].len() {
+        let op = rows[rows.len() - 1][i];
+        let mut val = match op {
+            "+" => 0,
+            "*" => 1,
+            _ => panic!("Unknown operation"),
+        };
+
+        for j in 0..rows.len() - 1 {
+            let num = rows[j][i].parse::<u64>().unwrap();
+            match op {
+                "+" => val += num,
+                "*" => val *= num,
+                _ => panic!("Unknown operation"),
+            }
+        }
+        sum += val;
+    }
+
     println!("Part 1: The grand total found by adding together all of the answers to the individual problems is {}", sum);
 }
