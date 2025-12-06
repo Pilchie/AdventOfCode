5756bf43c4cbaedc582c395476560dcf59ebc56a
Kevin Pilch <me@pilchie.com>
2025-12-06 00:43:07 +0000

    2025-12-05 - Part 2

--- 2025/5/cafeteria/src/main.rs
+++ 2025/5/cafeteria/src/main.rs
@@ -4,13 +4,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading the file");
-    part_one(&contents);
-    part_two(&contents);
-}
-
-fn part_one(contents: &str) {
-    let mut fresh = 0;
-
     let mut ranges = Vec::new();
     for line in contents.lines() {
         if line.trim().is_empty() {
@@ -23,9 +16,21 @@ fn part_one(contents: &str) {
     }
     ranges.sort_by_key(|r| *r.start());
 
+    let mut ingredients = Vec::new();
     for line in contents.lines().skip(ranges.len() + 1) {
         let ingredient = line.parse::<u64>().unwrap();
-        for range in &ranges {
+        ingredients.push(ingredient);
+    }
+
+    part_one(&ranges, ingredients);
+    part_two(&ranges);
+}
+
+fn part_one(ranges: &[RangeInclusive<u64>], ingredients: Vec<u64>) {
+    let mut fresh = 0;
+
+    for ingredient in ingredients {
+        for range in ranges {
             if range.contains(&ingredient) {
                 fresh += 1;
                 break;
@@ -35,6 +40,25 @@ fn part_one(contents: &str) {
     println!("Part 1: {} of the available ingredient IDs are fresh", fresh);
 }
 
-fn part_two(contents: &str) {
-    
+fn part_two(ranges: &[RangeInclusive<u64>]) {
+    let final_ranges = merge_ranges(ranges);
+    let count: u64 = final_ranges.iter().map(|r| r.end() - r.start() + 1).sum();
+    println!("Part 2: {} ingredient IDs are considered to be fresh according to the fresh ingredient ID ranges", count);
+}
+
+fn merge_ranges(ranges: &[RangeInclusive<u64>]) -> Vec<RangeInclusive<u64>> {
+    let mut merged_ranges: Vec<RangeInclusive<u64>> = Vec::new();
+
+    for range in ranges {
+        if let Some(last_range) = merged_ranges.last_mut() {
+            if *range.start() <= *last_range.end() + 1 {
+                let new_end = (*last_range.end()).max(*range.end());
+                *last_range = RangeInclusive::new(*last_range.start(), new_end);
+                continue;
+            }
+        }
+        merged_ranges.push(range.clone());
+    }
+
+    merged_ranges
 }
